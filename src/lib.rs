c96788a74903c410877ab6c193f8e2b9959effd5
damszew <damian.szewczyk111@gmail.com>
2020-03-14 14:56:45 +0100

    Use responsive sidebar

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
 use seed::{prelude::*, *};
 
 struct Model {
-    sidebar: bool,
     page: Page,
 }
 
 impl Default for Model {
     fn default() -> Self {
         Self {
-            sidebar: true,
             page: Page::AboutMe,
         }
     }
@@ -23,58 +21,50 @@ enum Page {
 
 #[derive(Clone)]
 enum Msg {
-    ToggleSidebar,
     ChangePage(Page),
 }
 
 fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
     match msg {
-        Msg::ToggleSidebar => model.sidebar = !model.sidebar,
         Msg::ChangePage(page) => model.page = page,
     }
 }
 
-fn view(model: &Model) -> impl View<Msg> {
-    vec![
-        // Sidebar
+fn sidebar_view() -> Node<Msg> {
+    div![
+        attrs![ At::Class => "sidebar" ],
         div![
-            attrs![ At::Class => "sidebar" ],
-            button![
-                attrs![At::Class => "menu-icon"],
-                simple_ev(Ev::Click, Msg::ToggleSidebar),
-                "☰"
+            a![
+                attrs![ At::Href => "#about_me", At::Class => "active" ],
+                simple_ev(Ev::Click, Msg::ChangePage(Page::AboutMe)),
+                "About Me"
             ],
-            if model.sidebar {
-                div![
-                    attrs![ At::Class => "menu" ],
-                    ul![
-                        li![a![
-                            attrs![ At::Href => "#about_me" ],
-                            simple_ev(Ev::Click, Msg::ChangePage(Page::AboutMe)),
-                            "About Me"
-                        ]],
-                        li![a![
-                            attrs![ At::Href => "#projects" ],
-                            simple_ev(Ev::Click, Msg::ChangePage(Page::Projects)),
-                            "Pet projects"
-                        ]],
-                        li![a![
-                            attrs![ At::Href => "#contact" ],
-                            simple_ev(Ev::Click, Msg::ChangePage(Page::Contact)),
-                            "Contact"
-                        ]],
-                    ]
-                ]
-            } else {
-                div![]
+            a![
+                attrs![ At::Href => "#projects" ],
+                simple_ev(Ev::Click, Msg::ChangePage(Page::Projects)),
+                "Pet projects"
+            ],
+            a![
+                attrs![ At::Href => "#contact" ],
+                simple_ev(Ev::Click, Msg::ChangePage(Page::Contact)),
+                "Contact"
+            ],
+        ]
+    ]
+}
+
+fn view(model: &Model) -> Vec<Node<Msg>> {
+    vec![
+        sidebar_view(),
+        // Pages
+        div![
+            attrs![ At::Class => "content" ],
+            match model.page {
+                Page::AboutMe => "AboutMe",
+                Page::Projects => "Projects",
+                Page::Contact => "Contact",
             }
         ],
-        // Pages
-        div![match model.page {
-            Page::AboutMe => "AboutMe",
-            Page::Projects => "Projects",
-            Page::Contact => "Contact",
-        }],
     ]
 }
 

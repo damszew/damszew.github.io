668ce83d037471ac27873fe020e0734ad0d58536
damszew <damian.szewczyk111@gmail.com>
2020-04-02 17:25:36 +0200

    Make about_me page prettier

--- src/page/about.rs
+++ src/page/about.rs
@@ -3,9 +3,13 @@ use seed::{prelude::*, *};
 
 pub fn view() -> impl View<Msg> {
     div![
-        attrs!(At::Class => "about_me"),
-        h1!["Damian Szewczyk"],
-        img![attrs!(At::Src => "https://picsum.photos/450")],
-        p!["Beginner Rustacean with passion of learning new technologies"],
+        attrs!(At::Class => "about_me clearfix container"),
+        img![attrs!(At::Src => "https://picsum.photos/450",  At::Class => "box2")],
+        div![
+            attrs!(At::Class => "box1"),
+            h1!["Damian Szewczyk"],
+            p![attrs!(At::Class => "social_icons"), "[LinkedIn] [GitHub]"],
+            p!["Beginner Rustacean with passion of learning new technologies"],
+        ],
     ]
 }

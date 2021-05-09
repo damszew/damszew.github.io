c6a0246c78cd2ce2a6d42f013584a3ed3b8f038d
damszew <damian.szewczyk111@gmail.com>
2021-05-09 20:36:47 +0200

    Add support for tailwindcss

---
+++ style/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './src/**/*.rs',
+    './*.html',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
